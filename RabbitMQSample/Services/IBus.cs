e945afad35746252eb2e26bb163f05454cd52087
Faiz Fasasi <faizfasasi@gmail.com>
2021-08-17 18:04:58 +0100

    remove commented code

--- RabbitMQSample/Services/IBus.cs
+++ RabbitMQSample/Services/IBus.cs
@@ -8,6 +8,5 @@ namespace RabbitMQSample.Services
     public interface IBus
     {
         Task SendAsync<T>(string queue, T message);
-        //Task ReceiveAsync<T>(string queue, Action<T> onMessage);
     }
 }
--- RabbitMQSample/Services/RabbitBus.cs
+++ RabbitMQSample/Services/RabbitBus.cs
@@ -29,22 +29,5 @@ namespace RabbitMQSample.Services
                     Encoding.UTF8.GetBytes(output));
             });
         }
-
-        //public async Task ReceiveAsync<T>(string queue, Action<T> onMessage)
-        //{
-        //    _channel.QueueDeclare(queue, true, false, false, null);
-        //    var consumer = new AsyncEventingBasicConsumer(_channel);
-
-        //    consumer.Received += async (s, e) =>
-        //    {
-        //        var jsonSpecified = Encoding.UTF8.GetString(e.Body.Span);
-        //        var item = JsonConvert.DeserializeObject<T>(jsonSpecified);
-        //        onMessage(item);
-        //        await Task.Yield();
-        //    };
-
-        //    _channel.BasicConsume(queue, true, consumer);
-        //    await Task.Yield();
-        //}
     }
 }
